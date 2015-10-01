86855f63ef180af878c21a21c4eb154db04d7b67
leepedark <leepedark@gmail.com>
2015-10-01 15:06:44 +0900

    마이페이지수정

--- src/service/LoginService.java
+++ src/service/LoginService.java
@@ -5,14 +5,12 @@ import java.sql.SQLException;
 import java.util.List;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
 import repository.LoginDao;
 import vo.GoodsVo;
-import vo.LoginVo;
 
 public class LoginService {
 	private LoginService(){}
